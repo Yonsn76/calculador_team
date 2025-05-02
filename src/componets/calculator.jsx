2860bf6bbe081d4786dc42188382ead1b3be7817
TomsonLu <retobloyons@gmail.com>
2025-05-02 10:29:19 -0500

    Revert "corrección de error en components"

--- src/componets/calculator.jsx
+++ src/componets/calculator.jsx
@@ -21,11 +21,6 @@ export const Calculator = () => {
     } else if (value === "=") {
       setInput(evaluateExpression(input));
     } else {
-      const lastChar = input[input.length - 1];
-      const operators = ["/", "*", "-", "+"];
-      if (operators.includes(value) && operators.includes(lastChar)) {
-        return;
-      }
       setInput((prev) => prev + value);
     }
   };
