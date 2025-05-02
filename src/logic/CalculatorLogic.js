c71f31e4044980e4ec0c81ac6daf32936d2caed2
Jordy-Dev <jordy.16182@gmail.com>
2025-05-02 08:56:07 -0500

    Inicio

---
+++ src/logic/CalculatorLogic.js
@@ -0,0 +1,15 @@
+export const evaluateExpression = (expression) => {
+
+    try {
+   
+     const result = eval(expression); 
+   
+     return result.toString();
+   
+    } catch {
+   
+     return "Error";
+   
+    }
+   
+   };
