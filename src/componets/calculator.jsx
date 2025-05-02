e6fbbd55ee2b84a26b5c79b6ce4cd8c2a3427943
Yonsn76 <retobloyons@gmail.com>
2025-05-02 08:48:49 -0500

    Componentes agregados

---
+++ src/componets/calculator.jsx
@@ -0,0 +1,37 @@
+// src/components/Calculator.jsx
+import { useState } from "react";
+import { Button } from "./Button";
+import { evaluateExpression } from "../logic/CalculatorLogic";
+import "../styles/Calculator.css";
+
+const buttons = [
+  "7", "8", "9", "/",
+  "4", "5", "6", "*",
+  "1", "2", "3", "-",
+  "0", "C", "=", "+"
+];
+
+export const Calculator = () => {
+  const [input, setInput] = useState("");
+
+  const handleClick = (value) => {
+    if (value === "C") {
+      setInput("");
+    } else if (value === "=") {
+      setInput(evaluateExpression(input));
+    } else {
+      setInput((prev) => prev + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">{input || "0"}</div>
+      <div className="buttons">
+        {buttons.map((btn, idx) => (
+          <Button key={idx} value={btn} onClick={handleClick} />
+        ))}
+      </div>
+    </div>
+  );
+};
